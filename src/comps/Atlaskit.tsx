3d602bac3a07c261eb39b427714fec67c1308a4f
Colton Colcleasure <colshacol@gmail.com>
2020-11-01 21:08:48 -0600

    Atlaskit sucks for exports

--- src/comps/Atlaskit.tsx
+++ src/comps/Atlaskit.tsx
@@ -1,3 +1,25 @@
 import Spinner from "@atlaskit/spinner"
+import Button from "@atlaskit/button"
+import { colors } from "@atlaskit/theme"
+import Popup from "@atlaskit/popup"
+import Avatar from "@atlaskit/avatar"
+import TextField from "@atlaskit/textfield"
+import ButtonGroup from "@atlaskit/button/button-group"
+import StandardButton from "@atlaskit/button/standard-button"
+import Select from "@atlaskit/select"
+import Lozenge from "@atlaskit/lozenge"
+import { Checkbox } from "@atlaskit/checkbox"
 
-export { Spinner }
+export {
+  Spinner,
+  Button,
+  colors,
+  Popup,
+  Avatar,
+  TextField,
+  ButtonGroup,
+  StandardButton,
+  Select,
+  Lozenge,
+  Checkbox,
+}
